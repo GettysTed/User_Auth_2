9daef3d13d132a6064ab35470ff1778c62500579
GettysTed <kerrth02@gettysburg.edu>
2012-10-09 21:06:33 -0400

    Login errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,12 +13,15 @@ class SessionsController < ApplicationController
   end
   
   def login_attempt
+    @attempt = true
+    @error_message = "Invalid Username or Password"
     authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
       flash[:notice] = "Welcome again, you logged in as #(authorized_user.username)"
       redirect_to(:action => 'profile')
     else
+      @attempt = false
       flash[:notice] = "Invalid Username or Password"
       flash[:color] = "invalid"
       render "login"
